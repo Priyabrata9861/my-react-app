ad1ef8f9615d180bf27accf6f421eb4ff20c9de2
Priyabrata <priyabratamishra2001@gmail.com>
2024-02-26 16:03:29 +0530

    milu

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,123 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate , Link } from "react-router-dom";
 import axios from "axios";
-import "@trendmicro/react-sidenav/dist/react-sidenav.css";
-import "./Lo.css";
-import { Link } from "react-router-dom";
+import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 
-
-const Dashboard = () => <div>Dashboard Component</div>;
-const Users = () => <div>Users Component</div>;
-const Client = () => <div>Client Component</div>;
-const ClientManagement = () => <div>Client Management Component</div>;
-const Job = () => <div>Job Component</div>;
-const Reports = () => <div>Reports Component</div>;
-const SiaqAdmin = () => <div>Siaq Admin Component</div>;
-
-const Sidebar = () => {
-  const [isActive, setIsActive] = useState(false);
+const UserList = () => {
+  const [users, setUsers] = useState([]);
   const navigate = useNavigate();
-  const [activeDropdown, setActiveDropdown] = useState(null);
-
-  const toggleSidebar = () => {
-    setIsActive((prevIsActive) => !prevIsActive);
-  };
-  const handleDropdownItemClick = (dropdownId) => {
-    setActiveDropdown(activeDropdown === dropdownId ? null : dropdownId);
-  };
+  
+  
   const handleLogOut = () => {
     localStorage.removeItem("token");
     navigate("/");
     alert("LogOut Successfully");
   };
-
-  return (
-    <div>
-      <div>
-        <button onClick={handleLogOut}>LogOut</button>
-      </div>
-      <div className={`sidebar ${isActive ? "active" : ""}`}>
-        <div className="toggle-btn" onClick={toggleSidebar}>
-          ☰
-        </div>
-        <ul className="sidebar-menu">
-          <li>
-            <Link to="/dashboard">Dashboard</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/client">Client</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to="/clientmanagement">Client Management</Link>
-              </li>
-              <li>
-                <Link to="#">Building Management</Link>
-              </li>
-              <li>
-                <Link to="#">Client Representative</Link>
-              </li>
-              <li>
-                <Link to="#">Client Statistics</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/job">Job</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to="#">Job Management</Link>
-              </li>
-              <li>
-                <Link to="#">Upload CSV</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/reports">Reports</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to="#">Shortcodes</Link>
-              </li>
-              <li>
-                <Link to="#">Report Templates</Link>
-              </li>
-              <li>
-                <Link to="#">Manage Reports</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/siaqadmin">Siaq Admin</Link>
-            <ul className="dropdown">
-              <li>
-                <Link to="#">Role</Link>
-              </li>
-              <li>
-                <Link to="#">Menu</Link>
-              </li>
-              <li>
-                <Link to="#">Role Menu</Link>
-              </li>
-              <li>
-                <Link to="#">Manage Survey Threshold</Link>
-              </li>
-              <li>
-                <Link to="#">Designation</Link>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-const UserList = () => {
-  const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
@@ -137,10 +32,43 @@ const UserList = () => {
 
   return (
     <div>
-      <Sidebar />
-      
-       
-      
+      <Sidebar>
+  <Menu
+    menuItemStyles={{
+      button: {
+        [`&.active`]: {
+          backgroundColor: '#13395e',
+          color: '#b6c8d9',
+        },
+      },
+    }}
+  >
+  <MenuItem omponent={<Link to="/Dashboard" />}> Dashboard </MenuItem>
+  <MenuItem> Users </MenuItem>
+    <SubMenu label="Clients">
+      <MenuItem> Client Management </MenuItem>
+      <MenuItem> Building Management </MenuItem>
+      <MenuItem> Client Representative </MenuItem>
+      <MenuItem> Client Representative </MenuItem>
+    </SubMenu>
+    <SubMenu label="Jobs">
+      <MenuItem> Job Management </MenuItem>
+      <MenuItem> Upload CSV </MenuItem>
+    </SubMenu>
+    <SubMenu label="Reports">
+      <MenuItem> Shortcodes </MenuItem>
+      <MenuItem> Report Templates </MenuItem>
+      <MenuItem> Manage Reports </MenuItem>
+    </SubMenu>
+    <SubMenu label="SIAQ Admin">
+      <MenuItem> Role </MenuItem>
+      <MenuItem> Menu </MenuItem>
+      <MenuItem> Role Menu </MenuItem>
+      <MenuItem> Manage Survey Threshold </MenuItem>
+      <MenuItem> Designation </MenuItem>
+    </SubMenu>
+  </Menu>
+</Sidebar>;  
     </div>
   );
 };
